d1bc3d1a7816d53197c6ad27a6d029e5107218c6
Ravindu Bandara <135025413+Rxvxndu2003@users.noreply.github.com>
2024-08-21 17:29:18 +0530

    Update home.blade.php

--- field_spark_update/resources/views/pages/home.blade.php
+++ field_spark_update/resources/views/pages/home.blade.php
@@ -408,7 +408,62 @@ ga('create', 'UA-30027142-1', 'w3layouts.com');
 		</div>
 	</div>
 <!-- //newsletter -->
-
+<!-- footer -->
+<div class="footer">
+	    <div class="container">
+			<div class="w3agile_footer_grids">
+				<div class="col-md-3 agileinfo_footer_grid">
+					<div class="agileits_w3layouts_footer_logo">
+					<h2><a href="/"><span>F</span>eildSpark<i>Grow healthy products</i></a></h2>
+					</div>
+				</div>
+				<div class="col-md-4 agileinfo_footer_grid">
+					<h3>Contact Info</h3>
+					<h4>Call Us <span>+94 713300619</span></h4>
+					<p>Field Spark,No.78, Main Road, Kegalle. <span>71000 Sri Lanka.</span></p>
+					<ul class="agileits_social_list">
+						<li><a href="#" class="w3_agile_facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+						<li><a href="#" class="agile_twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+						<li><a href="#" class="w3_agile_dribble"><i class="fa fa-dribbble" aria-hidden="true"></i></a></li>
+						<li><a href="#" class="w3_agile_vimeo"><i class="fa fa-vimeo" aria-hidden="true"></i></a></li>
+					</ul>
+				</div>
+				<div class="col-md-2 agileinfo_footer_grid agileinfo_footer_grid1">
+					<h3>Navigation</h3>
+					<ul class="w3layouts_footer_nav">
+						<li><a href="/"><i class="fa fa-long-arrow-right" aria-hidden="true"></i>Home</a></li>
+						<li><a href="/aboutus"><i class="fa fa-long-arrow-right" aria-hidden="true"></i>About Us</a></li>
+						<li><a href="/services"><i class="fa fa-long-arrow-right" aria-hidden="true"></i>Services</a></li>
+						<li><a href="/contactus"><i class="fa fa-long-arrow-right" aria-hidden="true"></i>Contact Us</a></li>
+					</ul>
+				</div>
+				<div class="col-md-3 agileinfo_footer_grid">
+					<h3>Blog Posts</h3>
+					<div class="agileinfo_footer_grid_left">
+						<a href="#" data-toggle="modal" data-target="#myModal"><img src="images/6.jpg" alt=" " class="img-responsive" /></a>
+					</div>
+					<div class="agileinfo_footer_grid_left">
+						<a href="#" data-toggle="modal" data-target="#myModal"><img src="images/2.jpg" alt=" " class="img-responsive" /></a>
+					</div>
+					<div class="agileinfo_footer_grid_left">
+						<a href="#" data-toggle="modal" data-target="#myModal"><img src="images/5.jpg" alt=" " class="img-responsive" /></a>
+					</div>
+					<div class="agileinfo_footer_grid_left">
+						<a href="#" data-toggle="modal" data-target="#myModal"><img src="images/3.jpg" alt=" " class="img-responsive" /></a>
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+		<div class="w3_agileits_footer_copy">
+			<div class="container">
+				<p>Field Spark 2024</p>
+				<p>	&copy; Powered by 4GBx</p>
+			</div>
+		</div>
+</div>
+<!-- //footer -->
 <!-- stats -->
 	<script src="js/jquery.waypoints.min.js"></script>
 	<script src="js/jquery.countup.js"></script>
