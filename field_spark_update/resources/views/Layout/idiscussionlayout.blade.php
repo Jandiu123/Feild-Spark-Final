dc2665bfb06c92783d7f9694cc85f885a1ccab05
Rxvxndu2003 <cb012195@students.apiit.lk>
2024-08-23 17:38:15 +0530

    refactor finished

--- field_spark_update/resources/views/Layout/idiscussionlayout.blade.php
+++ field_spark_update/resources/views/Layout/idiscussionlayout.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Field Spark Discussion Forum - Farmers</title>
+    <title>Field Spark Discussion Forum - Instructors</title>
    
 </head>
 
@@ -75,6 +75,16 @@
 		}
 	</script>
 <!-- //menu -->
-<script type="text/javascript">window.$crisp=[];window.CRISP_WEBSITE_ID="b48ca7c7-c3fc-4bf5-acf7-c6bbc1bc1e37";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();</script>
+<script type="text/javascript">
+        window.$crisp = [];
+        window.CRISP_WEBSITE_ID = "b48ca7c7-c3fc-4bf5-acf7-c6bbc1bc1e37";
+        (function() {
+            const d = document;
+            const s = d.createElement("script");
+            s.src = "https://client.crisp.chat/l.js";
+            s.async = 1;
+            d.getElementsByTagName("head")[0].appendChild(s);
+        })();
+</script>
 </body>
 </html>
